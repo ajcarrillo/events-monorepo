c03f73cef386b018538d7d401b81173d82cd995b
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2023-08-26 03:22:47 -0500

    feat: Create home and profile pages

---
+++ apps/client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+        <h1 className="tw-text-3xl tw-font-bold tw-underline">
+            Hello world!
+        </h1>
+    </div>
+    )
+}
+
+export default Home
---
+++ apps/client/src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile: React.FC = () => {
+  return (
+    <div>
+        <h1>Perfil</h1>
+        <p>Esta es la página de perfil</p>
+    </div>
+  )
+}
+
+export default Profile;
